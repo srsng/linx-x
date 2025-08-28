5f85af296a90e44056122f2294b90bdcfc450516
srsng <srsnng@hotmail.com>
2025-08-28 11:49:35 +0800

    refactor(core): 移除CDN和媒体处理模块及相关工具
    清理不再使用的CDN服务和媒体处理功能代码，包括相关工具类和初始化逻辑

--- src/mcp_server/core/__init__.py
+++ src/mcp_server/core/__init__.py
@@ -1,7 +1,5 @@
 from .storage import load as load_storage
 from .version import load as load_version
-# from .cdn import load as load_cdn
-# from .media_processing import load as load_media_processing
 
 
 def load():
@@ -9,7 +7,3 @@ def load():
     load_version()
     # 存储业务 - 注册会话感知的工具
     load_storage()
-    # # CDN
-    # load_cdn()
-    # # 智能多媒体
-    # load_media_processing()
